85a9b534833f4ab55ea19032ae6f0de36fa87629
Joanna Skowron <j.skowron89@gmail.com>
2019-10-10 10:56:20 +0200

    Add calendar week view

--- src/content/calendar/CalendarTaskListDay.js
+++ src/content/calendar/CalendarTaskListDay.js
@@ -4,6 +4,51 @@ import '../../styles/calendar/CalendarTaskListDay.css';
 class CalendarTaskListDay extends Component {
   state = {}
 
+  testData = [
+    {
+      id: 1,
+      title: "Test task title 1",
+      dateStart: new Date('10.03.2019 11:00:00'),
+      dateEnd: new Date('10.03.2019 12:00:00'),
+      description: "This is fake description for this test task. This is fake description for this test task. This is fake description for this test task. This is fake description for this test task. This is fake description for this test task. This is fake description for this test task. This is fake description for this test task. This is fake description for this test task. This is fake description for this test task. This is fake description for this test task. This is fake description for this test task. This is fake description for this test task. This is fake description for this test task. This is fake description for this test task. This is fake description for this test task.",
+    },
+    {
+      id: 2,
+      title: "Test task title 2",
+      dateStart: new Date('10.03.2019 16:00:00'),
+      dateEnd: new Date('10.03.2019 17:00:00'),
+      description: "This is fake description for this test task.",
+    },
+    {
+      id: 3,
+      title: "Test task title 3",
+      dateStart: new Date('10.03.2019 13:00:00'),
+      dateEnd: new Date('10.03.2019 13:30:00'),
+      description: "This is fake description for this test task.",
+    },
+    {
+      id: 4,
+      title: "Test task title 4",
+      dateStart: new Date('10.03.2019 14:00:00'),
+      dateEnd: new Date('10.03.2019 14:30:00'),
+      description: "This is fake description for this test task. This is fake description for this test task. This is fake description for this test task. This is fake description for this test task. This is fake description for this test task. This is fake description for this test task. This is fake description for this test task. This is fake description for this test task. This is fake description for this test task. This is fake description for this test task. This is fake description for this test task. This is fake description for this test task. This is fake description for this test task. This is fake description for this test task. This is fake description for this test task.",
+    },
+    {
+      id: 5,
+      title: "Test task title 5",
+      dateStart: new Date('10.03.2019 13:30:00'),
+      dateEnd: new Date('10.03.2019 14:00:00'),
+      description: "This is fake description for this test task.",
+    },
+    {
+      id: 6,
+      title: "Test task title 6",
+      dateStart: new Date('10.03.2019 07:00:00'),
+      dateEnd: new Date('10.03.2019 09:30:00'),
+      description: "This is fake description for this test task.",
+    },
+  ]
+
   generateDivs = (count, cls, ifClock) => {
     let divs = [];
     for (let i = 0; i < count; i++) {
@@ -18,9 +63,30 @@ class CalendarTaskListDay extends Component {
     return weekdays[day];
   }
 
+  renderTasks = () => {
+    const tasks = this.testData.map(task => {
+
+      const positionFromTopOfContainer = `${(task.dateStart.getHours() * 60) + task.dateStart.getMinutes()}px`;
+      const height = `${(task.dateEnd - task.dateStart) / (60 * 1000)}px`;
+      const style = {
+        top: positionFromTopOfContainer,
+        height: height,
+      };
+
+      return (
+        <div key={task.id} className="calendar-tasklist-day-task" style={style}>
+          <div className="calendar-tasklist-day-title-container">
+            <h1 className="calendar-tasklist-day-task-title">{task.title}</h1>
+            <p className="calendar-tasklist-day-task-date">{task.dateStart.toLocaleTimeString()} - {task.dateEnd.toLocaleTimeString()}</p>
+          </div>
+          <p className="calendar-tasklist-day-task-description">{task.description}</p>
+        </div>
+      )
+    })
+    return tasks;
+  }
+
   render() {
-
-
     return (
 
       <>
@@ -39,7 +105,7 @@ class CalendarTaskListDay extends Component {
             <div className="calendar-tasklist-day-table-item calendar-tasklist-item-first"></div>
             {this.generateDivs(23, 'calendar-tasklist-day-table-item', false)}
 
-
+            {this.renderTasks()}
           </div>
 
         </div>
--- src/content/calendar/CalendarTaskListWeek.js
+++ src/content/calendar/CalendarTaskListWeek.js
@@ -15,6 +15,51 @@ class CalendarTaskListWeek extends Component {
     6: 6
   }
 
+  testData = [
+    {
+      id: 1,
+      title: "Test task title 1",
+      dateStart: new Date('10.07.2019 11:00:00'),
+      dateEnd: new Date('10.07.2019 12:00:00'),
+      description: "This is fake description for this test task. This is fake description for this test task. This is fake description for this test task. This is fake description for this test task. This is fake description for this test task. This is fake description for this test task. This is fake description for this test task. This is fake description for this test task. This is fake description for this test task. This is fake description for this test task. This is fake description for this test task. This is fake description for this test task. This is fake description for this test task. This is fake description for this test task. This is fake description for this test task.",
+    },
+    {
+      id: 2,
+      title: "Test task title 2",
+      dateStart: new Date('10.08.2019 16:00:00'),
+      dateEnd: new Date('10.08.2019 17:00:00'),
+      description: "This is fake description for this test task.",
+    },
+    {
+      id: 3,
+      title: "Test task title 3",
+      dateStart: new Date('10.09.2019 13:00:00'),
+      dateEnd: new Date('10.09.2019 13:30:00'),
+      description: "This is fake description for this test task.",
+    },
+    {
+      id: 4,
+      title: "Test task title 4",
+      dateStart: new Date('10.09.2019 14:00:00'),
+      dateEnd: new Date('10.09.2019 14:30:00'),
+      description: "This is fake description for this test task. This is fake description for this test task. This is fake description for this test task. This is fake description for this test task. This is fake description for this test task. This is fake description for this test task. This is fake description for this test task. This is fake description for this test task. This is fake description for this test task. This is fake description for this test task. This is fake description for this test task. This is fake description for this test task. This is fake description for this test task. This is fake description for this test task. This is fake description for this test task.",
+    },
+    {
+      id: 5,
+      title: "Test task title 5",
+      dateStart: new Date('10.09.2019 13:30:00'),
+      dateEnd: new Date('10.09.2019 14:00:00'),
+      description: "This is fake description for this test task.",
+    },
+    {
+      id: 6,
+      title: "Test task title 6",
+      dateStart: new Date('10.09.2019 07:00:00'),
+      dateEnd: new Date('10.09.2019 09:30:00'),
+      description: "This is fake description for this test task.",
+    },
+  ]
+
   generateDivs = (count, cls, ifClock) => {
     let divs = [];
     for (let i = 0; i < count; i++) {
@@ -26,7 +71,21 @@ class CalendarTaskListWeek extends Component {
   getFirstDayOfWeek = (date) => {
     let dateWeekDay = date.getDay();
     date.setDate(date.getDate() - this.dayOftheWeekMap[dateWeekDay] + 1);
-    return date.toLocaleDateString();
+    return date;
+  }
+
+  generateTitleDivs = (count, cls) => {
+    let divs = [];
+    const monday = new Date(this.getFirstDayOfWeek(this.props.selectedDate));
+    const monthNames = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+
+    for (let i = 0; i < count; i++) {
+      const fullDate = new Date(monday.setDate(monday.getDate() + i));
+      const day = fullDate.getDate();
+      const month = monthNames[fullDate.getMonth()];
+      divs.push(<div key={i} className={cls}>{day} {month.toUpperCase()}</div>)
+    }
+    return divs;
   }
 
   getLastDayOfWeek = (date) => {
@@ -41,14 +100,40 @@ class CalendarTaskListWeek extends Component {
     return weekdays[day];
   }
 
+  renderTasks = () => {
+    const tasks = this.testData.map(task => {
+
+      const positionFromTopOfContainer = `${(task.dateStart.getHours() * 60) + task.dateStart.getMinutes()}px`;
+      const positionFromLeftOfContainer = `${this.dayOftheWeekMap[task.dateStart.getDay()] * (100 / 7)}%`;
+      const height = `${(task.dateEnd - task.dateStart) / (60 * 1000)}px`;
+      const style = {
+        top: positionFromTopOfContainer,
+        left: positionFromLeftOfContainer,
+        height: height,
+      };
+
+      return (
+        <div key={task.id} className="calendar-tasklist-week-task" style={style}>
+          <div className="calendar-tasklist-week-title-container">
+            <h1 className="calendar-tasklist-week-task-title">{task.title}</h1>
+            <p className="calendar-tasklist-week-task-date">{task.dateStart.toLocaleTimeString()} - {task.dateEnd.toLocaleTimeString()}</p>
+          </div>
+          <p className="calendar-tasklist-week-task-description">{task.description}</p>
+        </div>
+      )
+    })
+    return tasks;
+  }
+
   render() {
 
     return (
 
       <>
-        <h1 className="calendar-tasklist-day-title">
-          {this.getFirstDayOfWeek(this.props.selectedDate)} - {this.getLastDayOfWeek(this.props.selectedDate)}
-        </h1>
+        <div className="calendar-tasklist-week-title">
+          {/* {this.getFirstDayOfWeek(this.props.selectedDate)} - {this.getLastDayOfWeek(this.props.selectedDate)} */}
+          {this.generateTitleDivs(7, "calendar-tasklist-week-title-item")}
+        </div>
         <div className="calendar-tasklist-week-container">
 
           <div className="calendar-tasklist-week-clock">
@@ -59,6 +144,12 @@ class CalendarTaskListWeek extends Component {
           <div className="calendar-tasklist-week-table">
             <div className="calendar-tasklist-week-table-item calendar-tasklist-item-first"></div>
             {this.generateDivs(23, 'calendar-tasklist-week-table-item', false)}
+            <div className="calendar-tasklist-week-days-container">
+              {this.generateDivs(7, 'calendar-tasklist-week-days-item', false)}
+
+              {this.renderTasks()}
+
+            </div>
           </div>
 
         </div>
