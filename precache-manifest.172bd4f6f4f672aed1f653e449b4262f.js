729df9425b313d5a8c40d87522ea1c69d0d8ef36
Joanna Skowron <j.skowron89@gmail.com>
2021-01-29 15:20:17 +0100

    Updates

---
+++ precache-manifest.172bd4f6f4f672aed1f653e449b4262f.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14d2d68317269e38e412931324c84885",
+    "url": "/mailing_app/index.html"
+  },
+  {
+    "revision": "592bb0da07ff5ed834cc",
+    "url": "/mailing_app/static/css/2.214e3fa5.chunk.css"
+  },
+  {
+    "revision": "633d06d8922923917bc6",
+    "url": "/mailing_app/static/css/main.bc5ce90c.chunk.css"
+  },
+  {
+    "revision": "592bb0da07ff5ed834cc",
+    "url": "/mailing_app/static/js/2.45c62ac9.chunk.js"
+  },
+  {
+    "revision": "648e0cf19ec644f1454296373a65e32f",
+    "url": "/mailing_app/static/js/2.45c62ac9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "633d06d8922923917bc6",
+    "url": "/mailing_app/static/js/main.82c9f233.chunk.js"
+  },
+  {
+    "revision": "beaac150457939ffe19c",
+    "url": "/mailing_app/static/js/runtime-main.fefd652e.js"
+  },
+  {
+    "revision": "7d0f383635592fcb9916ba47d78ceffd",
+    "url": "/mailing_app/static/media/avatar1.7d0f3836.png"
+  },
+  {
+    "revision": "b9546314b6f8e27d01f3e5af31db0566",
+    "url": "/mailing_app/static/media/avatar2.b9546314.png"
+  },
+  {
+    "revision": "5a4e7d16db1b1f267633ed3a6ebe0d82",
+    "url": "/mailing_app/static/media/avatar3.5a4e7d16.png"
+  },
+  {
+    "revision": "224f59a40df7e802f9c9b65d573ff406",
+    "url": "/mailing_app/static/media/avatar4.224f59a4.png"
+  },
+  {
+    "revision": "8e318d8a81e198289060acc9596c13a7",
+    "url": "/mailing_app/static/media/avatar5.8e318d8a.png"
+  },
+  {
+    "revision": "11b3efc7f805df2dcb586b549a9a633e",
+    "url": "/mailing_app/static/media/avatar6.11b3efc7.png"
+  },
+  {
+    "revision": "f139e58515531efa0091dd24b4138798",
+    "url": "/mailing_app/static/media/bgc-image1.f139e585.jpg"
+  }
+]);
